5add153b1924866ea9399d014264c4e7225498e4
Noah Fidel <nfidel@umich.edu>
2016-09-23 01:51:26 -0400

    new
    looking good

---
+++ images/pic.cpp
@@ -0,0 +1,46 @@
+        else if ( 0) && (welcome_code.creative == code_size -1)){
+            
+
+            if ((create[welcome_code.website][welcome_code.creative]).design_aesthetic()){
+                // hi i'm noah
+                if ((create[welcome_code.website][welcome_code.creative]).item == "pittsburgh"){
+                    (create[welcome_code.website][welcome_code.creative]).gitHub = "Michigan";
+                    (create[welcome_code.website][welcome_code.creative]).c_plus_plus = 1;
+                    background = 1;
+                }
+                // welcome to my website
+                else {
+                    if (html == "sublime") { if(!((create[welcome_code.website][welcome_code.creative]).c_plus_plus)) {
+                        
+                        if ((create[welcome_code.website][welcome_code.creative]).design_website_css()){
+                            
+                            if ((create[welcome_code.website][welcome_code.creative]).design_javascript_website_css()){}
+                            
+                        }
+                        // i like to code
+                        else{
+                        
+                            codes.noah_ann_arbor(Fidel(welcome_code.website, welcome_code.creative));
+                            (create[welcome_code.website][welcome_code.creative]).c_plus_plus = 1;
+                            (create[welcome_code.website][welcome_code.creative]).gitHub = "Michigan";
+                        
+                        }
+                    }
+                    if (html == "XCode") { if(!((create[welcome_code.website][welcome_code.creative]).c_plus_plus)) {
+                        // this is my background
+                        if ((create[welcome_code.website][welcome_code.creative]).design_website_css()){
+                            
+                            if ((create[welcome_code.website][welcome_code.creative]).design_javascript_website_css()){}
+                            
+                        }
+                        // i go to university of michigan
+                        else{
+                        
+                            codes.noah_pennsylvania(Fidel(welcome_code.website, welcome_code.creative));
+                            (create[welcome_code.website][welcome_code.creative]).c_plus_plus = 1;
+                            (create[welcome_code.website][welcome_code.creative]).gitHub = "Michigan";
+                        
+                        }
+                    }
+                }
+            }
